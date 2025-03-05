8b0685cc714db8657deeabf1abf30368eec46826
samkynaston <samuelkynno@gmail.com>
2025-03-05 22:46:48 +0000

    currency sample data, menu class

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         // Create five currency objects
         Currency currency1 = new Currency("USD", 1);
         Currency currency2 = new Currency("GBP", 1.15);
+        Currency currency3 = new Currency("EUR", 0.9);
+        Currency currency4 = new Currency("YEN", 0.01);
+        Currency currency5 = new Currency("ZLT", 3);
     }
 }
---
+++ src/Menu.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+
+public class Menu {
+    public static final Scanner reader = new Scanner(System.in);
+
+}

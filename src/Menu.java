e84f88b07955c79fcef4622c946d15f9f9f8f819
samkynaston <samuelkynno@gmail.com>
2025-03-05 22:50:08 +0000

    menu class constructor

--- src/Menu.java
+++ src/Menu.java
@@ -2,5 +2,17 @@ import java.util.Scanner;
 
 public class Menu {
     public static final Scanner reader = new Scanner(System.in);
+    public boolean isClosed = false;
 
+    public Menu() {
+        while (!isClosed) {
+            System.out.println("0. Close Menu");
+
+            int Input = reader.nextInt();
+
+            if (Input == 0) {
+                isClosed = false;
+            }
+        }
+    }
 }
